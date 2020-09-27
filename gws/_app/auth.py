6e9aed0e923b91c6c72fefda652b0c50fd306144
Djomangan Adama OUATTARA <douattara@gencovery.com>
2020-09-28 00:07:34 +0200

    Add login/logout

--- gws/_app/auth.py
+++ gws/_app/auth.py
@@ -25,37 +25,36 @@ settings = Settings.retrieve()
 template_dir = settings.get_template_dir("gws")
 templates = Jinja2Templates(directory=template_dir)
 
-@requires("authenticated", redirect='auth')
-async def userpage(request):
-    pass
-
 @requires("authenticated")
-async def authpage(request):
+async def loginpage(request):
     if request.user.is_authenticated:
         response = RedirectResponse(url='/')
         return response
     else:
         raise AuthenticationError('Not allowed')
 
+@requires("authenticated")
+async def logoutpage(request):
+    if request.user.is_authenticated:
+        request.session['token'] = None
+    
+    return templates.TemplateResponse("auth/logout.html", {
+        'request': request, 
+        'settings': settings,
+    })
+
+
+@requires("authenticated")
+async def profilepage(request):
+    return templates.TemplateResponse("auth/profile.html", {
+        'request': request, 
+        'settings': settings,
+    })
+
+
 class AuthBackend(AuthenticationBackend):
 
     async def authenticate(self, request):
-        #print(request.headers)
-        
-        #if "Authorization" not in request.headers:
-        #    return
-        #
-        #auth = request.headers["Authorization"]
-        #try:
-        #    scheme, credentials = auth.split()
-        #    if scheme.lower() != 'basic':
-        #        return
-        #    decoded = base64.b64decode(credentials).decode("ascii")
-        #except (ValueError, UnicodeDecodeError, binascii.Error) as err:
-        #    raise AuthenticationError(f'Invalid basic auth credentials. Error: {err}')
-        #
-        #email, _, password = decoded.partition(":")
-
         token = request.query_params.get("token", None)
         
         if token is None:
@@ -78,57 +77,3 @@ class AuthBackend(AuthenticationBackend):
 
         auth_credentials = AuthCredentials(scopes)
         return auth_credentials, user
-    
-# class Login(HTTPEndpoint):
-
-#     @async_crsf_protect
-#     async def get(self, request):
-#         template_dir = settings.get_template_dir(settings.name)
-#         templates = Jinja2Templates(directory=template_dir)
-#         return templates.TemplateResponse('user/login.html', { "request": request,  "settings": settings })
-
-#     @async_crsf_protect
-#     async def post(self, request):
-#         template_dir = settings.get_template_dir(settings.name)
-#         templates = Jinja2Templates(directory=template_dir)
-
-#         data = await request.form()
-#         email = data.get('email')
-#         password = data.get('password')
-        
-#         try:
-#             user = User.get(email=email, password=password)
-#             request.session["is_authenticated"] = True
-#             return request.redirect("/")
-#         except:
-#             return templates.TemplateResponse('user/login.html', { "request": request, "settings": settings }) 
-
-# class Signup(HTTPEndpoint):
-
-#     @async_crsf_protect
-#     async def get(self, request):
-#         template_dir = settings.get_template_dir(settings.name)
-#         templates = Jinja2Templates(directory=template_dir)
-#         return templates.TemplateResponse('user/signup.html', { "request": request, "settings": settings })
-
-#     @async_crsf_protect
-#     async def post(self, request):
-#         template_dir = settings.get_template_dir(settings.name)
-#         templates = Jinja2Templates(directory=template_dir)
-
-#         data = await request.form()
-#         username = data.get('username')
-#         email = data.get('email')
-#         tasks = BackgroundTasks()
-#         tasks.add_task(send_welcome_email, to_address=email)
-#         tasks.add_task(send_admin_notification, username=username)
-
-#         return templates.TemplateResponse('user/signup.html', { "request": request, "settings": settings }) 
-
-# async def send_welcome_email(to_address):
-#     subject = "Account validation"
-#     message = "Your account is created"
-#     await Email.send(to_address, subject, message)
-
-# async def send_admin_notification(username):
-#     pass
