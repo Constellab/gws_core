28db3355e4b393eb0db495fdbbd8652260e36e71
Djomangan Adama OUATTARA <douattara@gencovery.com>
2020-12-10 14:09:05 +0000

    Update three-js and palette-js src

--- web/static/js/g3-map/_g3/map.js
+++ web/static/js/g3-map/_g3/map.js
@@ -13,9 +13,9 @@ import * as FORCE from "./_layout/force.js";
 import * as UTILS from "../../_utils/files.js";
 import * as COLOR from '../../_utils/color.js'
 
-import * as THREE from '/static-dist/three-js/build/three.module.js';
-import { TrackballControls } from '/static-dist/three-js/examples/jsm/controls/TrackballControls.js';
-import { DragControls } from '/static-dist/three-js/examples/jsm/controls/DragControls.js';
+import * as THREE from '/three-js/build/three.module.js';
+import { TrackballControls } from '/three-js/examples/jsm/controls/TrackballControls.js';
+import { DragControls } from '/three-js/examples/jsm/controls/DragControls.js';
 
 export class Map {
 
--- web/static/js/prism/prism.module.js
+++
@@ -1,9 +0,0 @@
-/**
- * LICENSE
- * This software is the exclusive property of Gencovery SAS. 
- * The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
- * About us: https://gencovery.com
- */
-
-import * as PRISM from './_prism/all.js'
-
