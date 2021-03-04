f37fd5f719f007efa50938fc052a2d54df402a33
Djomangan Adama OUATTARA <douattara@gencovery.com>
2021-03-04 16:52:12 +0000

    Add dep folder

---
+++ dep/install.sh
@@ -0,0 +1,8 @@
+# LICENSE
+# This software is the exclusive property of Gencovery SAS. 
+# The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
+# About us: https://gencovery.com
+
+
+# Installation script executed on server first startup
+
