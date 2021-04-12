92c08b81f08cca6296ca7a2a89ee315c70f76468
Benjamin <benjamin.maisonneuve-ext@sii.fr>
2021-04-12 12:00:46 +0200

    [CORS] disable credentials

--- gws/_api/core_api.py
+++ gws/_api/core_api.py
@@ -34,7 +34,7 @@ app = FastAPI(docs_url="/docs")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
