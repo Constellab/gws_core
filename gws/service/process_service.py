6246b209b95d7c9a3ae955bca26cf29f2cdd2566
Benjamin <bmaisonneuve@gencovery.com>
2021-06-02 06:59:28 +0000

    [ProcessType] new route to get process type grouped by module

--- gws/service/process_service.py
+++ gws/service/process_service.py
@@ -1,24 +1,27 @@
 # LICENSE
-# This software is the exclusive property of Gencovery SAS. 
+# This software is the exclusive property of Gencovery SAS.
 # The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
 # About us: https://gencovery.com
 
-from typing import List
+from typing import Dict, List, Union
 
+from gws.dto.process_type_dto import ProcessTypeTree
+from gws.http import *
+from gws.model import Experiment, Process, ProgressBar
 from gws.query import Paginator
 from gws.typing import ProcessType
-from gws.model import Process, Experiment, ProgressBar
-from gws.http import *
+from peewee import ModelSelect
 
 from .base_service import BaseService
 
+
 class ProcessService(BaseService):
-    
+
     # -- F --
-    
+
     @classmethod
     def fetch_process(cls, type: str = "gws.model.Process", uri: str = "") -> Process:
-        
+
         from gws.service.model_service import ModelService
         t = None
         if type:
@@ -27,33 +30,36 @@ class ProcessService(BaseService):
                 raise HTTPNotFound(detail=f"Process type '{type}' not found")
         else:
             t = Process
-            
+
         try:
             p = t.get(t.uri == uri)
             return p
         except Exception as err:
-            raise HTTPNotFound(detail=f"No process found with uri '{uri}' and type '{type}'", debug_error=err)
-    
+            raise HTTPNotFound(
+                detail=f"No process found with uri '{uri}' and type '{type}'", debug_error=err)
+
     @classmethod
     def fetch_process_progress_bar(cls, type: str = "gws.model.Protocol", uri: str = "") -> ProgressBar:
         try:
-            return ProgressBar.get( (ProgressBar.process_uri == uri) & (ProgressBar.process_type == type) )  
+            return ProgressBar.get((ProgressBar.process_uri == uri) & (ProgressBar.process_type == type))
         except Exception as err:
-            raise HTTPNotFound(detail=f"No progress bar found with process_uri '{uri}' and process_type '{type}'", debug_error=err)
-                    
+            raise HTTPNotFound(
+                detail=f"No progress bar found with process_uri '{uri}' and process_type '{type}'", debug_error=err)
+
     @classmethod
-    def fetch_process_list(cls, \
-                           type = "gws.model.Process", \
-                           search_text: str="", \
-                           experiment_uri: str=None, \
-                           page: int=1, \
-                           number_of_items_per_page: int=20, \
-                           as_json = False) -> (Paginator, List[Process], List[dict], ):
-        
-        number_of_items_per_page = min(number_of_items_per_page, cls._number_of_items_per_page)
+    def fetch_process_list(cls,
+                           type="gws.model.Process",
+                           search_text: str = "",
+                           experiment_uri: str = None,
+                           page: int = 1,
+                           number_of_items_per_page: int = 20,
+                           as_json=False) -> Union[Paginator, List[Process], List[dict]]:
+
+        number_of_items_per_page = min(
+            number_of_items_per_page, cls._number_of_items_per_page)
 
         from gws.service.model_service import ModelService
-        
+
         t = None
         if type:
             t = ModelService.get_model_type(type)
@@ -61,7 +67,7 @@ class ProcessService(BaseService):
                 raise HTTPNotFound(detail=f"Process type '{type}' not found")
         else:
             t = Process
-        
+
         if search_text:
             Q = t.search(search_text)
             result = []
@@ -70,10 +76,11 @@ class ProcessService(BaseService):
                     result.append(o.get_related().to_json(shallow=True))
                 else:
                     result.append(o.get_related())
-            
-            P = Paginator(Q, page=page, number_of_items_per_page=number_of_items_per_page)
+
+            P = Paginator(
+                Q, page=page, number_of_items_per_page=number_of_items_per_page)
             return {
-                'data' : result,
+                'data': result,
                 'paginator': P._paginator_dict()
             }
         else:
@@ -81,32 +88,52 @@ class ProcessService(BaseService):
                 Q = t.select().order_by(t.creation_datetime.desc())
             else:
                 Q = t.select_me().order_by(t.creation_datetime.desc())
-                
+
             if experiment_uri:
                 Q = Q.join(Experiment, on=(t.experiment_id == Experiment.id))\
-                        .where(Experiment.uri == experiment_uri)
+                    .where(Experiment.uri == experiment_uri)
 
-            P = Paginator(Q, page=page, number_of_items_per_page=number_of_items_per_page)
+            P = Paginator(
+                Q, page=page, number_of_items_per_page=number_of_items_per_page)
             if as_json:
                 return P.to_json(shallow=True)
             else:
-                return P    
- 
+                return P
+
     @classmethod
-    def fetch_process_type_list(cls, \
-                                page: int=1, \
-                                number_of_items_per_page :int=20, \
-                                as_json = False) -> (Paginator, dict):
-        
-        Q = ProcessType.select()\
-                        .where(ProcessType.base_ptype=="gws.model.Process")\
-                        .order_by(ProcessType.ptype.desc())
- 
-        number_of_items_per_page = min(number_of_items_per_page, cls._number_of_items_per_page)
-        
-        P = Paginator(Q, page=page, number_of_items_per_page=number_of_items_per_page)
+    def fetch_process_type_list(cls,
+                                page: int = 1,
+                                number_of_items_per_page: int = 20,
+                                as_json=False) -> Union[Paginator, dict, List[ProcessType]]:
+
+        query = ProcessType.select()\
+            .where(ProcessType.base_ptype == "gws.model.Process")\
+            .order_by(ProcessType.ptype.desc())
+
+        number_of_items_per_page = min(
+            number_of_items_per_page, cls._number_of_items_per_page)
+        paginator = Paginator(
+            query, page=page, number_of_items_per_page=number_of_items_per_page)
         if as_json:
-            return P.to_json(shallow=True) 
+            return paginator.to_json(shallow=True)
         else:
-            return P
-            
+            return paginator
+
+    @classmethod
+    def fetch_process_type_grouped(cls) -> List[ProcessTypeTree]:
+        """
+        Return all the process types grouped by module and submodules
+        """
+
+        query: List[ProcessType] = ProcessType.select()\
+            .where(ProcessType.base_ptype == "gws.model.Process")\
+            .order_by(ProcessType.ptype.asc())
+
+        # create a fake main group to add processes in it
+        group: ProcessTypeTree = ProcessTypeTree('')
+
+        for process_type in query:
+            group.add_process_type(
+                process_type.get_ptypes_array(), process_type.to_json())
+
+        return group.sub_modules
--- gws/typing.py
+++ gws/typing.py
@@ -1,14 +1,19 @@
 # LICENSE
-# This software is the exclusive property of Gencovery SAS. 
+# This software is the exclusive property of Gencovery SAS.
 # The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
 # About us: https://gencovery.com
 
-from peewee import  CharField
+import json
+from typing import List, Union
+
+from peewee import CharField
 
 from gws.model import Viewable
 
+
 class Path:
     pass
+
 
 class URL:
     pass
@@ -19,6 +24,7 @@ class URL:
 #
 # ####################################################################
 
+
 class ProcessType(Viewable):
     """
     ProcessType class. This class allows storing information on all the types of the processes in the system.
@@ -28,15 +34,15 @@ class ProcessType(Viewable):
     :property base_ptype: The base type of the related process. The base type is either `gws.model.Process` or `gws.model.Protocol`
     :type base_ptype: `str`
     """
-    
-    ptype = CharField(null=True, index=True, unique=True)
-    base_ptype = CharField(null=True, index=True)
-    
+
+    ptype: str = CharField(null=True, index=True, unique=True)
+    base_ptype: str = CharField(null=True, index=True)
+
     _table_name = 'gws_process_type'
-    
-    def to_json(self, *, stringify: bool=False, prettify: bool=False, **kwargs) -> (str, dict, ):
+
+    def to_json(self, *, stringify: bool = False, prettify: bool = False, **kwargs) -> Union[str, dict]:
         from gws.service.model_service import ModelService
-        
+
         _json = super().to_json(**kwargs)
         model_t = ModelService.get_model_type(self.ptype)
         specs = model_t.input_specs
@@ -46,13 +52,13 @@ class ProcessType(Viewable):
             t_list = specs[name]
             if not isinstance(t_list, tuple):
                 t_list = (t_list, )
-            
+
             for t in t_list:
                 if t is None:
                     _json["input_specs"][name].append(None)
                 else:
                     _json["input_specs"][name].append(t.full_classname())
-        
+
         specs = model_t.output_specs
         _json["output_specs"] = {}
         for name in specs:
@@ -60,20 +66,20 @@ class ProcessType(Viewable):
             t_list = specs[name]
             if not isinstance(t_list, tuple):
                 t_list = (t_list, )
-            
+
             for t in t_list:
                 if t is None:
                     _json["output_specs"][name].append(None)
                 else:
                     _json["output_specs"][name].append(t.full_classname())
-                    
+
         _json["config_specs"] = model_t.config_specs
         for k in _json["config_specs"]:
             spec = _json["config_specs"][k]
             if "type" in spec and isinstance(spec["type"], type):
-                t_str = spec["type"].__name__ 
+                t_str = spec["type"].__name__
                 _json["config_specs"][k]["type"] = t_str
-        
+
         if stringify:
             if prettify:
                 return json.dumps(_json, indent=4)
@@ -81,12 +87,19 @@ class ProcessType(Viewable):
                 return json.dumps(_json)
         else:
             return _json
-        
+
+    def get_ptypes_array(self) -> List[str]:
+        """
+        Return the ptypes as an array by splitting with .
+        """
+        return self.ptype.split('.')
+
 # ####################################################################
 #
 # ResourceType class
 #
 # ####################################################################
+
 
 class ResourceType(Viewable):
     """
@@ -97,8 +110,8 @@ class ResourceType(Viewable):
     :property base_rtype: The base type of the related resource. The base type is either `gws.model.Resource`
     :type base_ptype: `str`
     """
-    
+
     rtype = CharField(null=True, index=True, unique=True)
     base_rtype = CharField(null=True, index=True)
-    
+
     _table_name = 'gws_resource_type'
