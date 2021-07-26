46b7975eec16c8abc329547b427d0b7bab4710b7
Benjamin <bmaisonneuve@gencovery.com>
2021-07-26 08:22:17 +0000

    [CI] Test CI/CD

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gws",
+    version="0.0.1",
+    author="Example Author",
+    author_email="admin@gencovery.com",
+    description="Core of Gencovery Web Service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "gws"},
+    packages=setuptools.find_packages(where="gws"),
+    python_requires=">=3.6",
+)
