1d9424859bb2b72f963b6acf095a7963f13d3017
Benjamin <bmaisonneuve@gencovery.com>
2021-07-26 09:42:54 +0000

    [CI] Test CI/CD

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gws",
+    name="gws_core",
     version="0.0.1",
     author="Example Author",
     author_email="admin@gencovery.com",
