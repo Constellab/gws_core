417d5ae57edb8fcedd86a96498e377c226f655fa
Benjamin <bmaisonneuve@gencovery.com>
2021-10-19 08:16:33 +0000

    (fix) fix test view

--- tests/table/test_barplot_view.py
+++ tests/table/test_barplot_view.py
@@ -1,6 +1,6 @@
 import os
 
-from gws_core import BaseTestCase, BarPlotView, Settings, Table
+from gws_core import BarPlotView, BaseTestCase, Settings, Table
 
 
 class TestBarPlotView(BaseTestCase):
@@ -10,13 +10,12 @@ class TestBarPlotView(BaseTestCase):
         testdata_dir = settings.get_variable("gws_core:testdata_dir")
         file_path = os.path.join(testdata_dir, "iris.csv")
         table = Table.import_from_path(file_path, delimiter=",", head=0)
-        vw = BarPlotView(data=table, title="Bar plot")
+        vw = BarPlotView(data=table)
 
         dic = vw.to_dict(x_column_name="sepal.length",
-                               y_column_names=["petal.length", "petal.width"],)
+                         y_column_names=["petal.length", "petal.width"],)
 
         self.assertEqual(dic["type"], "bar-plot-view")
-        self.assertEqual(dic["title"], "Bar plot")
 
         self.assertEqual(dic["series"][0]["data"]["x"], table.get_data()["sepal.length"].values.tolist())
         self.assertEqual(dic["series"][0]["data"]["y"], table.get_data()["petal.length"].values.tolist())
