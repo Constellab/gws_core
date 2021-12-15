79ea115569514153a17deca3bc4eeb6fa817fbf3
Djomangan OUATTARA <douattara@gencovery.com>
2021-12-15 12:43:03 +0000

    (refactor)[extra] expose the base model service in extra

--- src/gws_core/extra.py
+++ src/gws_core/extra.py
@@ -1,7 +1,1 @@
-
-from .impl.table.helper.constructor.data_scale_filter_param import \
-    DataScaleFilterParamConstructor as DataScaleFilterParamConstructor
-from .impl.table.helper.constructor.num_data_filter_param import \
-    NumericDataFilterParamConstructor as NumericDataFilterParamConstructor
-from .impl.table.helper.constructor.text_data_filter_param import \
-    TextDataFilterParamConstructor as TextDataFilterParamConstructor
+from .core.model.base_model_service import BaseModelService as BaseModelService
