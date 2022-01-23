5d60014b506a675255deaf4cc315f4c64c819a42
Djomangan OUATTARA <douattara@gencovery.com>
2022-01-23 15:53:49 +0000

    (fix)[json] json exporter creates JSONFile

--- src/gws_core/impl/json/json_tasks.py
+++ src/gws_core/impl/json/json_tasks.py
@@ -9,10 +9,10 @@ from typing import Type
 
 from ...config.config_types import ConfigParams, ConfigSpecs
 from ...config.param_spec import BoolParam, StrParam
-from ...impl.file.file import File
 from ...task.converter.exporter import ResourceExporter, exporter_decorator
 from ...task.converter.importer import ResourceImporter, importer_decorator
 from .json_dict import JSONDict
+from .json_file import JSONFile
 
 # ####################################################################
 #
@@ -21,11 +21,11 @@ from .json_dict import JSONDict
 # ####################################################################
 
 
-@importer_decorator(unique_name="JSONImporter", target_type=JSONDict)
+@importer_decorator(unique_name="JSONImporter", source_type=JSONFile, target_type=JSONDict)
 class JSONImporter(ResourceImporter):
     config_specs: ConfigSpecs = {'file_format': StrParam(default_value=".json", short_description="File format")}
 
-    async def import_from_path(self, file: File, params: ConfigParams, target_type: Type[JSONDict]) -> JSONDict:
+    async def import_from_path(self, file: JSONFile, params: ConfigParams, target_type: Type[JSONDict]) -> JSONDict:
         with open(file.path, "r", encoding="utf-8") as f:
             json_data = target_type()
             json_data.data = json.load(f)
@@ -39,7 +39,7 @@ class JSONImporter(ResourceImporter):
         # ####################################################################
 
 
-@exporter_decorator("JSONExporter", source_type=JSONDict)
+@exporter_decorator("JSONExporter", source_type=JSONDict, target_type=JSONFile)
 class JSONExporter(ResourceExporter):
     config_specs: ConfigSpecs = {
         'file_name': StrParam(default_value='file.json', short_description="Destination file name in the store"),
@@ -47,7 +47,7 @@ class JSONExporter(ResourceExporter):
         'prettify': BoolParam(default_value=False, short_description="True to indent and prettify the JSON file, False otherwise")
     }
 
-    async def export_to_path(self, resource: JSONDict, dest_dir: str, params: ConfigParams, target_type: Type[File]) -> File:
+    async def export_to_path(self, resource: JSONDict, dest_dir: str, params: ConfigParams, target_type: Type[JSONFile]) -> JSONFile:
         file_path = os.path.join(dest_dir, params.get_value('file_name', 'file.json'))
 
         with open(file_path, "w", encoding="utf-8") as f:
@@ -56,4 +56,4 @@ class JSONExporter(ResourceExporter):
             else:
                 json.dump(resource.data, f)
 
-        return File(file_path)
+        return JSONFile(file_path)
