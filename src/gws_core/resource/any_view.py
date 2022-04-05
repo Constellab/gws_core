ed15da55767ebc8af72f398ce4dc36cc22381612
Benjamin <bmaisonneuve@gencovery.com>
2022-04-05 14:40:11 +0000

    (fix) fix AnyView

--- src/gws_core/resource/any_view.py
+++ src/gws_core/resource/any_view.py
@@ -16,7 +16,7 @@ class AnyView(View):
     _view_json: Dict
 
     def __init__(self, view_json: Dict):
-        super().__init__(view_json.get("data"))
+        super().__init__()
         self._type = view_json.get("type")
         self._view_json = view_json
 
