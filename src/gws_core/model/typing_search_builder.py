90c2cef742e15e0148dfa899f7804a854270d540
Benjamin <bmaisonneuve@gencovery.com>
2022-05-19 15:49:34 +0000

    (feat)[Typing] improve search

--- src/gws_core/model/typing_search_builder.py
+++ src/gws_core/model/typing_search_builder.py
@@ -1,11 +1,7 @@
 
 
-from typing import List, Type
+from typing import Type
 
-from gws_core.core.utils.utils import Utils
-from gws_core.model.typing_manager import TypingManager
-from gws_core.resource.resource import Resource
-from gws_core.task.task_typing import TaskTyping
 from peewee import Expression
 from playhouse.mysql_ext import Match
 
@@ -28,15 +24,6 @@ class TypingSearchBuilder(SearchBuilder):
                 (self._model_type.human_name, self._model_type.short_description, self._model_type.unique_name),
                 filter_['value'],
                 modifier='IN BOOLEAN MODE')
-        # Special case to filter on relate_model and parent of the model
-        elif filter_['key'] == 'related_model_typing_name':
-
-            if issubclass(self._model_type, TaskTyping):
-                related_model_type: Type[Typing] = TypingManager.get_type_from_name(filter_['value'])
-
-                return self._model_type.get_related_model_expression(related_model_type)
-            else:
-                return None
 
         elif filter_['key'] == 'include_deprecated':
             if not filter_['value']:
--- src/gws_core/model/typing_service.py
+++ src/gws_core/model/typing_service.py
@@ -1,10 +1,12 @@
 
 
+from importlib.resources import Resource
 from typing import Callable, Type
 
 from gws_core.core.classes.paginator import Paginator
 from gws_core.core.classes.search_builder import SearchBuilder, SearchParams
 from gws_core.model.typing import Typing, TypingNameObj
+from gws_core.model.typing_manager import TypingManager
 from gws_core.model.typing_search_builder import TypingSearchBuilder
 from gws_core.protocol.protocol_typing import ProtocolTyping
 from gws_core.resource.resource_typing import ResourceTyping
@@ -31,9 +33,10 @@ class TypingService():
     @classmethod
     def search(cls, search: SearchParams,
                page: int = 0, number_of_items_per_page: int = 20,
-               type_: Type[Typing] = Typing) -> Paginator[Typing]:
+               search_builder: TypingSearchBuilder = None) -> Paginator[Typing]:
 
-        search_builder: SearchBuilder = TypingSearchBuilder(type_)
+        if search_builder is None:
+            search_builder = TypingSearchBuilder(Typing)
 
         # force to add a filter hide to False
         search.override_filter_criteria('hide', 'EQ', False)
@@ -46,17 +49,22 @@ class TypingService():
     def search_importers(cls, resource_typing_name: str, extension: str,
                          search: SearchParams,
                          page: int = 0, number_of_items_per_page: int = 20) -> Paginator[Typing]:
+
+        search_builder = TypingSearchBuilder(TaskTyping)
+
         # force to add a filter on related typing name
-        search.override_filter_criteria('related_model_typing_name', 'EQ', resource_typing_name)
+        related_model_type: Type[Resource] = TypingManager.get_type_from_name(resource_typing_name)
+        search_builder.add_expression(TaskTyping.get_related_model_expression(related_model_type))
 
         # force to add a filter on sub type
         importer_sub_type: TaskSubType = 'IMPORTER'
-        search.override_filter_criteria('object_sub_type', 'EQ', importer_sub_type)
+        search_builder.add_expression(TaskTyping.object_sub_type == importer_sub_type)
 
+        # get the importer_ignore_extension and remove it
         ignore_file_extension: bool = search.get_filter_criteria_value('importer_ignore_extension')
         search.remove_filter_criteria('importer_ignore_extension')
 
-        pagination = cls.search(search, page, number_of_items_per_page, TaskTyping)
+        pagination = cls.search(search, page, number_of_items_per_page, search_builder)
 
         # filter the pagination by extension
         if extension and not ignore_file_extension:
@@ -71,13 +79,17 @@ class TypingService():
     def search_transformers(cls, resource_typing_name: str,
                             search: SearchParams,
                             page: int = 0, number_of_items_per_page: int = 20) -> Paginator[Typing]:
+
+        search_builder = TypingSearchBuilder(TaskTyping)
+
         # force to add a filter on related typing name
-        search.override_filter_criteria('related_model_typing_name', 'EQ', resource_typing_name)
+        related_model_type: Type[Resource] = TypingManager.get_type_from_name(resource_typing_name)
+        search_builder.add_expression(TaskTyping.get_related_model_expression(related_model_type))
 
         # force to add a filter on sub type
         sub_type: TaskSubType = 'TRANSFORMER'
-        search.override_filter_criteria('object_sub_type', 'EQ', sub_type)
+        search_builder.add_expression(TaskTyping.object_sub_type == sub_type)
 
-        pagination = cls.search(search, page, number_of_items_per_page)
+        pagination = cls.search(search, page, number_of_items_per_page, search_builder)
 
         return pagination
