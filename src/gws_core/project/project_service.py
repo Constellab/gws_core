6f7cfb85509a1f846995d0bdfdc9687468035524
Benjamin <bmaisonneuve@gencovery.com>
2022-06-06 09:34:50 +0000

    (feat)[Project] update project info if there were changed

--- src/gws_core/project/project_service.py
+++ src/gws_core/project/project_service.py
@@ -16,14 +16,33 @@ class ProjectService(BaseService):
 
         central_projects: List[ProjectDto] = cls.get_central_projects()
 
-        lab_projects: List[ProjectDto] = cls.get_lab_projects()
+        lab_projects: List[Project] = cls.get_lab_projects()
 
         # Add the lab projects to central project and avoid duplicate
         for lab_project in lab_projects:
-            if lab_project not in central_projects:
-                central_projects.append(lab_project)
+            # find the project in central by id
+            central_project = [p for p in central_projects if p.id == lab_project.id]
+
+            # if the project exist in central, refresh the project info if necessary
+            if len(central_project) >= 0:
+                cls._refresh_lab_project(lab_project, central_project[0])
+            # if the project is in the lab but not in central (should not happen=, add it to the list to return
+            else:
+                central_projects.append(
+                    ProjectDto(
+                        id=lab_project.id, title=lab_project.title,
+                        description=lab_project.description))
 
         return central_projects
+
+    @classmethod
+    def _refresh_lab_project(cls, lab_project: Project, central_project: ProjectDto) -> None:
+        """ Update the project store in the lab if information where change in central"""
+        if central_project.title != lab_project.title or \
+                central_project.description != lab_project.description:
+            lab_project.title = central_project.title
+            lab_project.description = central_project.description
+            lab_project.save()
 
     @classmethod
     def get_central_projects(cls) -> List[ProjectDto]:
@@ -45,13 +64,8 @@ class ProjectService(BaseService):
         return projects_dto
 
     @classmethod
-    def get_lab_projects(cls) -> List[ProjectDto]:
-        projects: List[Project] = list(Project.select())
-
-        projects_dto: List[ProjectDto] = []
-        for project in projects:
-            projects_dto.append(ProjectDto(id=project.id, title=project.title, description=project.description))
-        return projects_dto
+    def get_lab_projects(cls) -> List[Project]:
+        return list(Project.select())
 
     @classmethod
     def get_or_create_project_from_dto(cls, project_dto: ProjectDto) -> Project:
