756d1052da77374d7d0bfb9f973701e81e0a8647
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2023-03-03 15:10:38 +0000

    (fix) manage.py for new version

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ import sys
 if 'gws_core' not in sys.modules:
     core_lib_path = "/lab/user/bricks/gws_core/src"
     if not os.path.exists(core_lib_path):
-        core_lib_path = "/lab/user/bricks/.lib/gws_core/src"
+        core_lib_path = "/lab/.sys/bricks/gws_core/src"
         if not os.path.exists(core_lib_path):
             raise Exception("Cannot find gws_core brick")
     sys.path.insert(0, core_lib_path)
