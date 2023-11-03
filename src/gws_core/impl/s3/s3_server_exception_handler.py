2227719ee0c75cea26025788334c4ffb42f9248f
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2023-11-03 10:09:28 +0000

    (fix)[S3-server] add method to exception handler log

--- src/gws_core/impl/s3/s3_server_exception_handler.py
+++ src/gws_core/impl/s3/s3_server_exception_handler.py
@@ -39,7 +39,7 @@ class S3ServerExceptionHandler():
         :param exc: the exception
         :return: the response
         """
-        route_info: str = f" - Route: {request.url}" if request is not None else ""
+        route_info: str = f" - Route: {request.method} {request.url}" if request is not None else ""
 
         Logger.info(f"Handle s3 exception - {exc.code} {route_info} - {exc.message}")
         return ResponseHelper.create_xml_response(xml_text=exc.to_xml(), status_code=exc.status_code)
@@ -53,7 +53,7 @@ class S3ServerExceptionHandler():
         :param exc: the exception
         :return: the response
         """
-        route_info: str = f" - Route: {request.url}" if request is not None else ""
+        route_info: str = f" - Route: {request.method} {request.url}" if request is not None else ""
 
         Logger.info(f"Handle s3 http exception - {exc.status_code} {route_info} - {exc.detail}")
         s3_error = S3ServerException.from_http_exception(exc)
@@ -63,7 +63,7 @@ class S3ServerExceptionHandler():
     def _handle_unexcepted_exception(cls, request: Request, exception: Exception) -> Response:
         Logger.log_exception_stack_trace(exception)
 
-        route_info: str = f" - Route: {request.url}" if request is not None else ""
+        route_info: str = f" - Route: {request.method} {request.url}" if request is not None else ""
         Logger.error(f"Unexpected s3 exception - {route_info} - {str(exception)}")
 
         s3_error = S3ServerException.from_exception(exception)
