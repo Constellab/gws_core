6e919dfd58a0610f570cfc37520be62942df3029
vfoex <valentinfoex@gmail.com>
2024-01-05 15:46:26 +0000

    (feat)[LiveTask] generate live task json file

--- src/gws_core/code/task_generator_controller.py
+++ src/gws_core/code/task_generator_controller.py
@@ -20,3 +20,13 @@ def generate_task_code_from_live_task(id: str,
 
     # create a file response
     return ResponseHelper.create_file_response_from_str(code, 'task_code.py')
+
+
+@core_app.post("/task-generator/live-task-file/{id}", tags=["Task generator"],
+               summary="generate live task task file from live task")
+def generate_live_task_file_from_live_task(id: str,
+                                      _=Depends(AuthService.check_user_access_token)) -> StreamingResponse:
+    code = TaskGeneratorService.generate_live_task_file_from_live_task_id(id)
+
+    # create a file response
+    return ResponseHelper.create_file_response_from_str(code, 'live_task_file.json')
