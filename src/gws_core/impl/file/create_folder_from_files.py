546d78ca9b67b73ec6077e89f27e5b45081c2335
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-06-26 07:23:00 +0000

    (feat)[CreateFolderFromFile] new task to generate a folder from files

---
+++ src/gws_core/impl/file/create_folder_from_files.py
@@ -0,0 +1,79 @@
+
+
+import os
+from typing import List
+
+from gws_core.config.config_types import ConfigSpecs
+from gws_core.config.param.param_set import ParamSet
+from gws_core.config.param.param_spec import StrParam
+from gws_core.core.utils.settings import Settings
+from gws_core.impl.file.file_helper import FileHelper
+from gws_core.impl.file.folder import Folder
+from gws_core.impl.file.fs_node import FSNode
+from gws_core.io.dynamic_io import DynamicInputs
+from gws_core.io.io_spec import InputSpec, OutputSpec
+from gws_core.io.io_specs import InputSpecs, OutputSpecs
+from gws_core.resource.resource_set.resource_list import ResourceList
+
+from ...config.config_params import ConfigParams
+from ...io.io_specs import InputSpecs, OutputSpecs
+from ...task.task import Task
+from ...task.task_decorator import task_decorator
+from ...task.task_io import TaskInputs, TaskOutputs
+
+
+@task_decorator(unique_name="folder_1_path", human_name="Create folder from files",
+                short_description="Create a folder from a list of files or folders.")
+class CreateFolderFromFiles(Task):
+    """
+    Create a folder from a list of files or folders.
+    Provide a list of files or folders as input and the task will create a folder containing the files or folders.
+
+    The input file and folder are copied to the new folder.
+
+    ## Configuration:
+    List of filenames to use for the files or folders in the input list.
+    If not provided, the original filenames will be used.
+    The filenames must be provided in the same order as the input list.
+
+    Ex: The second filename overide the name of the second input file or folder.
+    """
+
+    input_specs: InputSpecs = DynamicInputs(
+        additionnal_port_spec=InputSpec(FSNode, human_name="File or folder"))
+    output_specs = OutputSpecs({'folder': OutputSpec(Folder, human_name='Folder')})
+
+    config_specs: ConfigSpecs = {
+        'folder_name': StrParam(human_name="Output folder name", short_description="Name of the folder to create", optional=True),
+        'filenames': ParamSet({'filename': StrParam(
+            human_name="Filename", short_description="Overide the file or folder name at this index", optional=True)}, optional=True)}
+
+    def run(self, params: ConfigParams, inputs: TaskInputs) -> TaskOutputs:
+
+        temp_dir: str = None
+        folder_name = params.get_value('folder_name')
+        if folder_name:
+            temp_dir = os.path.join(Settings.get_root_temp_dir(), folder_name)
+            FileHelper.create_dir_if_not_exist(temp_dir)
+        else:
+            temp_dir = Settings.get_root_temp_dir()
+
+        configs: List[dict] = params.get_value('filenames')
+
+        i = 0
+        resource_list: ResourceList = inputs['source']
+        for resource in resource_list.get_resources():
+            if not isinstance(resource, FSNode):
+                self.log_error_message(f"Resource {i} is not a file nor a folder")
+                continue
+
+            node_name: str = None
+            if len(configs) > i and configs[i] and configs[i]['filename']:
+                node_name = configs[i]['filename']
+            else:
+                node_name = resource.get_base_name()
+
+            FileHelper.copy_node(resource.path, os.path.join(temp_dir, node_name))
+            i += 1
+
+        return {'folder': Folder(temp_dir)}
---
+++ tests/test_gws_core/file/test_create_folder_from_file.py
@@ -0,0 +1,71 @@
+
+
+import os
+
+from gws_core.core.utils.settings import Settings
+from gws_core.impl.file.create_folder_from_files import CreateFolderFromFiles
+from gws_core.impl.file.file import File
+from gws_core.impl.file.file_helper import FileHelper
+from gws_core.impl.file.folder import Folder
+from gws_core.impl.file.fs_node import FSNode
+from gws_core.io.dynamic_io import DynamicInputs
+from gws_core.io.io_spec import InputSpec
+from gws_core.task.task_runner import TaskRunner
+from gws_core.test.base_test_case_light import BaseTestCaseLight
+
+
+# test_create_folder_from_file
+class TestCreateFolderFromFile(BaseTestCaseLight):
+
+    def test_create_folder_from_file(self):
+
+        temp_dir = Settings.make_temp_dir()
+
+        file_1_path = FileHelper.create_empty_file_if_not_exist(os.path.join(temp_dir, 'test_file.txt'))
+        # write some content to the file
+        with open(file_1_path, 'w', encoding='UTF-8') as f:
+            f.write('Hello')
+
+        folder_1_path = FileHelper.create_dir_if_not_exist(os.path.join(temp_dir, 'test_folder'))
+        # create a file in the folder
+        FileHelper.create_empty_file_if_not_exist(os.path.join(folder_1_path, 'sub_file.txt'))
+
+        file = File(str(file_1_path))
+        folder = Folder(str(folder_1_path))
+
+        task_runner = TaskRunner(CreateFolderFromFiles,
+                                 params={'folder_name': 'new_folder',
+                                         'filenames': [
+                                             {'filename': ''},
+                                             {'filename': 'new_folder'},
+                                         ]},
+                                 inputs={'resource_1': file,
+                                         'resource_2': folder
+                                         },
+                                 input_specs=DynamicInputs({
+                                     'resource_1': InputSpec(FSNode),
+                                     'resource_2': InputSpec(FSNode),
+                                 })
+                                 )
+
+        outputs = task_runner.run()
+
+        folder_result: Folder = outputs.get('folder')
+
+        self.assertEqual(folder_result.get_base_name(), 'new_folder')
+
+        self.assertIsInstance(folder_result, Folder)
+        self.assertTrue(FileHelper.exists_on_os(folder_result.path))
+        self.assertTrue(FileHelper.exists_on_os(os.path.join(folder_result.path, 'test_file.txt')))
+
+        # check file content
+        with open(os.path.join(folder_result.path, 'test_file.txt'), 'r', encoding='UTF-8') as f:
+            self.assertEqual(f.read(), 'Hello')
+
+        # check that the folder has been created
+        self.assertTrue(FileHelper.exists_on_os(os.path.join(folder_result.path, 'new_folder')))
+        self.assertTrue(FileHelper.exists_on_os(os.path.join(folder_result.path, 'new_folder', 'sub_file.txt')))
+
+        # Check that the original file and folder are still there
+        self.assertTrue(FileHelper.exists_on_os(file_1_path))
+        self.assertTrue(FileHelper.exists_on_os(folder_1_path))
