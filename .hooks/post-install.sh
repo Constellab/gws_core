bdc2523a78753690f7ed61e7aefbbd30ea653d0f
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-07-18 10:39:35 +0000

    (feat)[Pip] prepare file for building pip package

--- .hooks/post-install.sh
+++ .hooks/post-install.sh
@@ -1,9 +1,3 @@
-# LICENSE
-# This software is the exclusive property of Gencovery SAS. 
-# The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
-# About us: https://gencovery.com
-
-
 # Post-installation script executed after module installation
 
 echo "Installing post-install .sh hooks ..."
