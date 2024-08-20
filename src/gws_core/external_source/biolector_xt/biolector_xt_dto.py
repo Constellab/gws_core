9480c0db298d20c4c5f14e803ecc93fa0ff4c80a
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-08-20 09:07:34 +0000

    (feat) remove biolector and external source

--- src/gws_core/external_source/biolector_xt/biolector_xt_dto.py
+++
@@ -1,9 +0,0 @@
-
-
-from gws_core.core.model.model_dto import BaseModelDTO
-
-
-class CredentialsDataBiolector(BaseModelDTO):
-    """Format of the data for biolector credentials"""
-    endpoint_url: str
-    secure_channel: bool
