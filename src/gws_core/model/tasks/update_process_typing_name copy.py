01658ce955ada5850245368e323b6a1973fa3088
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-11-05 17:01:22 +0000

    (feat)[TypingNameTask] new task to update resource or process typing name

---
+++ src/gws_core/model/tasks/update_process_typing_name copy.py
@@ -0,0 +1,48 @@
+from gws_core.config.config_params import ConfigParams
+from gws_core.config.config_types import ConfigSpecs
+from gws_core.config.param.param_set import ParamSet
+from gws_core.config.param.param_spec import StrParam
+from gws_core.core.db.sql_migrator import SqlMigrator
+from gws_core.model.typing import Typing
+from gws_core.model.typing_style import TypingStyle
+from gws_core.task.task import Task
+from gws_core.task.task_decorator import task_decorator
+from gws_core.task.task_io import TaskInputs, TaskOutputs
+
+
+@task_decorator("UpdateProcessTypingName", human_name="[Support] Update process typing name",
+                short_description="Support task to update the typing name of processes in the database",
+                style=TypingStyle.material_icon(material_icon_name='support_agent'))
+class UpdateProcessTypingName(Task):
+    """
+    ⚠️⚠️⚠️ Use this task only if you know what you are doing, it can break the system.⚠️⚠️⚠️
+
+
+    Update the typing name of processes (task or protocol) in the database.
+    This is useful when the typing name of a process has changed after an update for example.
+    """
+
+    config_specs: ConfigSpecs = {
+        # this config is only set when calling this automatically
+        "processes": ParamSet({
+            'old_name': StrParam(human_name="Old process typing name", short_description="Ex: TASK.gws_core.MyOldTaskUniqueName"),
+            'new_name': StrParam(human_name="New process typing name", short_description="Ex: TASK.gws_core.MyNewTaskUniqueName"),
+        }, human_name="Processes to update"),
+    }
+
+    def run(self, params: ConfigParams, inputs: TaskInputs) -> TaskOutputs:
+
+        processes = params.get_value("processes")
+
+        for process in processes:
+            old_name = process['old_name']
+            new_name = process['new_name']
+
+            try:
+                SqlMigrator.rename_process_typing_name(Typing.get_db(), old_name, new_name)
+            except Exception as e:
+                self.log_error_message(f"Error while updating process typing name '{old_name}' to '{new_name}'")
+                self.log_error_message(str(e))
+            self.log_info_message(f"Process typing name '{old_name}' updated to '{new_name}'")
+
+        return {}
---
+++ src/gws_core/model/tasks/update_resource_typing_name.py
@@ -0,0 +1,48 @@
+from gws_core.config.config_params import ConfigParams
+from gws_core.config.config_types import ConfigSpecs
+from gws_core.config.param.param_set import ParamSet
+from gws_core.config.param.param_spec import StrParam
+from gws_core.core.db.sql_migrator import SqlMigrator
+from gws_core.model.typing import Typing
+from gws_core.model.typing_style import TypingStyle
+from gws_core.task.task import Task
+from gws_core.task.task_decorator import task_decorator
+from gws_core.task.task_io import TaskInputs, TaskOutputs
+
+
+@task_decorator("UpdateResourceTypingName", human_name="[Support] Update resource typing name",
+                short_description="Support task to update the typing name of resources in the database",
+                style=TypingStyle.material_icon(material_icon_name='support_agent'))
+class UpdateResourceTypingName(Task):
+    """
+    ⚠️⚠️⚠️ Use this task only if you know what you are doing, it can break the system.⚠️⚠️⚠️
+
+
+    Update the typing name of resources in the database.
+    This is useful when the typing name of a resource has changed after an update for example.
+    """
+
+    config_specs: ConfigSpecs = {
+        # this config is only set when calling this automatically
+        "resources": ParamSet({
+            'old_name': StrParam(human_name="Old resource typing name", short_description="Ex: RESOURCE.gws_core.MyOldTaskUniqueName"),
+            'new_name': StrParam(human_name="New resource typing name", short_description="Ex: RESOURCE.gws_core.MyNewTaskUniqueName"),
+        }, human_name="Resources to update"),
+    }
+
+    def run(self, params: ConfigParams, inputs: TaskInputs) -> TaskOutputs:
+
+        resources = params.get_value("resources")
+
+        for resource in resources:
+            old_name = resource['old_name']
+            new_name = resource['new_name']
+
+            try:
+                SqlMigrator.rename_resource_typing_name(Typing.get_db(), old_name, new_name)
+            except Exception as e:
+                self.log_error_message(f"Error while updating resource typing name '{old_name}' to '{new_name}'")
+                self.log_error_message(str(e))
+            self.log_info_message(f"Resource typing name '{old_name}' updated to '{new_name}'")
+
+        return {}
