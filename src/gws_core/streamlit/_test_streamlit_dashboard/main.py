882d68534f874a3cc52ab604e9bf62fa8e2e85c9
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-11-05 09:09:26 +0000

    (feat)[StreamlitResourceSelect] improve default options, add ordering method

--- src/gws_core/streamlit/_test_streamlit_dashboard/main.py
+++ src/gws_core/streamlit/_test_streamlit_dashboard/main.py
@@ -10,15 +10,24 @@ from gws_core.streamlit import ResourceSearchInput
 
 st.title("Hello World")
 
-selected_resource: ResourceModel | None = ResourceSearchInput().add_resource_type_filter(
-    Folder).add_flagged_filter(True).add_is_archived_filter(False).select(placeholder="Search for folder")
+default_resource = ResourceModel.get_by_id_and_check('f3aa406d-bac6-468c-aa96-19b0cc0b5ad9')
 
+selected_resource: ResourceModel | None = ResourceSearchInput().add_flagged_filter(
+    True).add_is_archived_filter(False) \
+    .add_order_by(ResourceModel.name) \
+    .select(placeholder="Search for folder")
 
-search_scenario_builder = ScenarioSearchBuilder().add_tag_filter(
-    Tag(key='test', value='test')).add_is_archived_filter(False)
+if selected_resource:
+    st.write(selected_resource)
 
-list_scenario: List[Scenario] = search_scenario_builder.search_all()
+    if isinstance(selected_resource, ResourceModel):
+        st.write("Is ResourceModel")
 
-scenario_proxy = ScenarioProxy.from_existing_scenario("ID du scenario")
+# search_scenario_builder = ScenarioSearchBuilder().add_tag_filter(
+#     Tag(key='test', value='test')).add_is_archived_filter(False)
 
-scenario_proxy.add_to_queue()
+# list_scenario: List[Scenario] = search_scenario_builder.search_all()
+
+# scenario_proxy = ScenarioProxy.from_existing_scenario("ID du scenario")
+
+# scenario_proxy.add_to_queue()
