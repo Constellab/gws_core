7e1e55606ce4af20a0933bae2bf12480ebaaa252
vFoexGencoery <vfoex@gencovery.com>
2025-01-17 16:11:41 +0000

    (fix)[TestDynamicParams] fix test dynamic params

--- tests/test_gws_core/task/test_dynamic_param.py
+++ tests/test_gws_core/task/test_dynamic_param.py
@@ -20,17 +20,19 @@ class TestDynamicParam(BaseTestCase):
         proto = proto.refresh()
 
         ProtocolService.add_dynamic_param_spec_of_process(
-            proto.id, process_model.instance_name, 'params', 'a', IntParam(default_value=2).to_dto()
+            proto.id, process_model.instance_name, 'params', 'a', IntParam(default_value=2, optional=True).to_dto()
         )
 
         proto = proto.refresh()
 
         test_process_model = proto.get_process(process_model.instance_name)
+        test_process_model.set_config_values(
+            {'params': {'a': None}, 'code': test_process_model.config.get_value('code')})
         self.assertIsNotNone(test_process_model)
         self.assertIsNotNone(test_process_model.config.get_spec(PyAgent.CONFIG_PARAMS_NAME).specs['a'])
         self.assertIsNotNone(test_process_model.config.get_value('params'))
         self.assertEqual(test_process_model.config.get_spec('params').specs['a'].to_dto()
-                         .to_json_dict(), IntParam(default_value=2).to_dto().to_json_dict())
+                         .to_json_dict(), IntParam(default_value=2, optional=True).to_dto().to_json_dict())
         self.assertEqual(test_process_model.config.get_value('params')['a'], 2)
 
         process_model = proto.get_process('task')
@@ -42,28 +44,32 @@ class TestDynamicParam(BaseTestCase):
         proto = proto.refresh()
 
         test_process_model = proto.get_process(process_model.instance_name)
+        test_process_model.set_config_values(
+            {'params': {'a': None}, 'code': test_process_model.config.get_value('code')})
         self.assertIsNotNone(test_process_model)
         self.assertIsNotNone(test_process_model.config.get_spec('params').specs['a'])
         self.assertIsNotNone(test_process_model.config.get_value('params'))
-        self.assertEqual(test_process_model.config.get_spec('params')
-                         .specs['a'].to_dto().to_json_dict(), IntParam(default_value=3).to_dto().to_json_dict())
+        self.assertEqual(test_process_model.config.get_spec('params') .specs['a'].to_dto(
+        ).to_json_dict(), IntParam(default_value=3, optional=True).to_dto().to_json_dict())
         self.assertEqual(test_process_model.config.get_value('params')['a'], 3)
 
         process_model = proto.get_process('task')
 
         ProtocolService.rename_and_update_dynamic_param_spec_of_process(
-            proto.id, process_model.instance_name, 'params', 'a', 'b', IntParam(default_value=4).to_dto()
+            proto.id, process_model.instance_name, 'params', 'a', 'b', IntParam(default_value=4, optional=True).to_dto()
         )
 
         proto = proto.refresh()
 
         test_process_model = proto.get_process(process_model.instance_name)
+        test_process_model.set_config_values(
+            {'params': {'b': None}, 'code': test_process_model.config.get_value('code')})
         self.assertIsNotNone(test_process_model)
         self.assertIsNotNone(test_process_model.config.get_spec('params').specs['b'])
         self.assertTrue('a' not in test_process_model.config.get_spec('params').specs)
         self.assertIsNotNone(test_process_model.config.get_value('params'))
-        self.assertEqual(test_process_model.config.get_spec('params')
-                         .specs['b'].to_dto().to_json_dict(), IntParam(default_value=4).to_dto().to_json_dict())
+        self.assertEqual(test_process_model.config.get_spec('params') .specs['b'].to_dto(
+        ).to_json_dict(), IntParam(default_value=4, optional=True).to_dto().to_json_dict())
         self.assertEqual(test_process_model.config.get_value('params')['b'], 4)
         self.assertTrue('a' not in test_process_model.config.get_values())
 
