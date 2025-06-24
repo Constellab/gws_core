0bd8c0bae3edcc3a8ecf5b79f763ff7e1c9449a4
vFoexGencoery <vfoex@gencovery.com>
2025-06-24 13:58:32 +0000

    (fix)[Tag] fix pull request for tag

--- src/gws_core/tag/tag_search_builder.py
+++ src/gws_core/tag/tag_search_builder.py
@@ -1,10 +1,8 @@
 
-from gws_core.tag.entity_with_tag_search_builder import \
-    EntityWithTagSearchBuilder
+from gws_core.core.classes.search_builder import SearchBuilder
 from gws_core.tag.tag_key_model import TagKeyModel
 
 
-class TagSearchBuilder(EntityWithTagSearchBuilder):
+class TagSearchBuilder(SearchBuilder):
     def __init__(self) -> None:
-        super().__init__(TagKeyModel, None,
-                         default_orders=[TagKeyModel.created_at.desc()])
+        super().__init__(TagKeyModel, default_orders=[TagKeyModel.created_at.desc()])
