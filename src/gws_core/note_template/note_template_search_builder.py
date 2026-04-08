e7a497b456be0bc7f79cabcce43d8eabdafeb5e3
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2026-04-08 14:38:02 +0000

    (feat)[NoteTemplate] add tags to NoteTemplate

--- src/gws_core/note_template/note_template_search_builder.py
+++ src/gws_core/note_template/note_template_search_builder.py
@@ -1,7 +1,8 @@
-from gws_core.core.classes.search_builder import SearchBuilder
 from gws_core.note_template.note_template import NoteTemplate
+from gws_core.tag.entity_with_tag_search_builder import EntityWithTagSearchBuilder
+from gws_core.tag.tag_entity_type import TagEntityType
 
 
-class NoteTemplateSearchBuilder(SearchBuilder):
+class NoteTemplateSearchBuilder(EntityWithTagSearchBuilder):
     def __init__(self) -> None:
-        super().__init__(NoteTemplate, default_orders=[NoteTemplate.last_modified_at.desc()])
+        super().__init__(NoteTemplate, TagEntityType.NOTE_TEMPLATE, default_orders=[NoteTemplate.last_modified_at.desc()])
